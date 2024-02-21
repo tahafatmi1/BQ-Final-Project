3f21d8e4392365d089a7bfd99246f391ea7241dd
tahafatmi1 <155869314+tahafatmi1@users.noreply.github.com>
2024-02-21 22:01:38 +0500

    Update script.js

--- script.js
+++ script.js
@@ -89,7 +89,32 @@ const questions = [
             {text: "A library provides specific functionality, while a framework provides a set of tools", correct: true},
         ]
     }   
-       
-  
     
 ]
+const questionElement = document.getElementById("question");
+const answerButton = document.getElementById("answer-buttons");
+const nextButton = document.getElementById("next-btn");
+
+let currentQuestionIndex = 0;
+let score = 0;
+
+function startQuiz() {
+    currentQuestionIndex = 0;
+    score = 0;
+    nextButton.innerHTML = "Next";
+    showQuestion();
+}
+function showQuestion() {
+    let currentQuestion = questions[currentQuestionIndex];
+    let questionNo = currentQuestionIndex + 1;
+    questionElement.innerHTML = questionNo + "." + currentQuestion.question;
+
+
+    currentQuestion.answers.forEach(answer =>{
+        const button = document.createElement("button");
+        button.innerHTML
+    }
+
+    )
+    
+}
