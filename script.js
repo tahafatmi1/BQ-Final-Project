7f270b40f8c6b8db38bd46f67e16f7555fa024f6
tahafatmi1 <155869314+tahafatmi1@users.noreply.github.com>
2024-02-21 19:49:52 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,95 @@
+const questions = [
+    {
+        question: "What does HTML stand for?",
+        answers: [ 
+            {text: "Hyperlink and Text Markup Language", correct: true},
+            {text: "Hyper Text Markup Language", correct: false},
+            {text: "High-Level Text Management Language", correct: false},
+            {text: "Hyper Transfer Markup Language", correct: false},   
+        ]
+    }         
+    {
+        question: " Which keyword is used to declare variables in JavaScript?",
+        answers: [ 
+            {text: "var", correct: false},
+            {text: "let", correct: false},
+            {text: "const", correct: false},
+            {text: "all of the above", correct: true},
+             
+        ]
+    }
+    {
+        question: " What is the purpose of media queries in CSS for responsive design?",
+        answers: [ 
+            {text: "To apply animations to elements", correct: false},
+            {text: "To detect the user's location", correct: false},
+            {text: "To define styles based on device characteristics", correct: true},
+            {text: "To create pop-up messages", correct: false},  
+        ]
+    }
+    {
+        question: "What is the primary purpose of version control systems like Git?",
+        answers: [ 
+            {text: "Java", correct: false},
+            {text: "Node Js", correct: true},
+            {text: "Python", correct: false},
+            {text: "Ruby", correct: false},
+        ]
+    }
+    {
+        question: " Which of the following is a server-side JavaScript runtime?",
+        answers: [ 
+            {text: "Managing project dependencies", correct: false},
+            {text: "Tracking changes in code over time", correct: true},
+            {text: "Formatting code for consistency", correct: false},
+            {text: "Creating visual designs for websites", correct: false},
+        ]
+    }
+    {
+        question: "Which protocol is commonly used to secure data transmitted over the web?",
+        answers: [ 
+            {text: "FTP", correct: false},
+            {text: "HTTP", correct: false},
+            {text: "TCP", correct: false},
+            {text: "HTTPS", correct: true},
+        ]
+    }  {
+        question: "What does API stand for in web development?",
+        answers: [ 
+            {text: "Application Programming Interface", correct: true},
+            {text: "Advanced Programming Interface", correct: false},
+            {text: "Automated Programming Interface", correct: false},
+            {text: "Associated Programming Interaction", correct: false},
+        ]
+    } 
+    {
+        question: "Which popular code editor is developed by Microsoft?",
+        answers: [ 
+            {text: "Atom", correct: false},
+            {text: "Sublime Text", correct: false},
+            {text: "Visual Studio Code", correct: true},
+            {text: "Eclipse", correct: false},
+        ]
+    }   
+    {
+        question: "What is a common technique for reducing image file sizes and improving web performance?",
+        answers: [ 
+            {text: "Image Cropping", correct: false},
+            {text: "Lazy Loading", correct: true},
+            {text: "Image Rotation", correct: false},
+            {text: "Image Enlargement", correct: false},
+        ]
+    }   
+    {
+        question: "What is the key difference between a framework and a library in web development?",
+        answers: [ 
+            {text: "A framework provides specific functionality, while a library provides a set of tools", correct: false},
+            {text: "Frameworks are only used in back-end development", correct: false},
+            {text: " Libraries are only used in front-end development", correct: false},
+            {text: "A library provides specific functionality, while a framework provides a set of tools", correct: true},
+        ]
+    }   
+       
+  
+    
+]
